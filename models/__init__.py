93e22838dad9d329ec7e10bbe260fa9f435e67b9
Anne Wachana <96468875+a11y-2824@users.noreply.github.com>
2022-11-29 03:49:04 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Create an init dunder """
-from models.engine.file_storage import FileStorage
-
-
-=======
 """ Import modules and packages """
 from models.engine.file_storage import FileStorage
 
->>>>>>> refs/remotes/origin/main
 storage = FileStorage()
 storage.reload()
